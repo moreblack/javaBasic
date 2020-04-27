a70765809d75c41ef9d1cc995f041ad8348fbf8c
MinKi <moreblack@naver.com>
2020-04-27 11:51:44 +0900

    new

--- BitReverseOperatorExample.java
+++ BitReverseOperatorExample.java
@@ -1,32 +1,35 @@
+import javax.sound.sampled.SourceDataLine;
 
 public class BitReverseOperatorExample {
-	
+
 	public static void main(String[] args) {
 		int num1 = 10;
 		int num2 = ~num1;
 		int num3 = num2 + 1;
-		
-		//java add....
-		
+		int num4 = 10000000;
+
+
+	system.out.println("Hello,world")
+
+		// java add....
+
 		String returnValue = toBinaryString(num1);
 		System.out.println(returnValue);
-		
+
 		String returnValue1 = toBinaryString(num2);
 		System.out.println(returnValue);
-		
+
 		String returnValue2 = toBinaryString(num3);
 		System.out.println(returnValue2);
 	}
 
-    public static String toBinaryString(int val) {
+	public static String toBinaryString(int val) {
 		String str = Integer.toBinaryString(val);
-		while(str.length() <32){
+		while (str.length() < 32) {
 			str += "0" + str;
-			
+
 		}
 		return str;
 	}
 
-
 }
-
