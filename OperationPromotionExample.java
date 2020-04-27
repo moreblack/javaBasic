0471eae3ac57489d6a28261dcdc52da5b352c41e
MinKi <moreblack@naver.com>
2020-04-27 10:39:33 +0900

    new

---
+++ OperationPromotionExample.java
@@ -0,0 +1,26 @@
+
+public class OperationPromotionExample {
+	public static void main(String[] args) {
+		byte byteValue1 = 10;
+		byte byteValue2 = 20;
+
+		int intValue1 = byteValue1 + byteValue2;
+		System.out.println(intValue1);
+		
+		char charValue1 = 'A';
+		char charValue2 = 1;
+
+		int inVaule2 = charValue1 + charValue2;
+		System.out.println("유니코드=" + intVaule2);
+		System.out.println("출력문자=" + (char)intValue2);
+		
+		int inValue3 = 10;
+		int inValue4 = inValue3/4;
+		System.out.println(inValue4);
+		
+		int intValue5 = 10;
+	
+		double doubleValue = intvalue5 / 4.0;
+		System.out.println(doubleValue);
+	}
+}
